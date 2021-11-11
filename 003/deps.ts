cf845d52c5e6fef73e592e338d6b2a3eb4286303
Kazuya HATTORI <boushi-bird@users.noreply.github.com>
2021-11-11 14:49:16 +0900

    deps.tsの使い方追加

---
+++ 003/deps.ts
@@ -0,0 +1,6 @@
+export { format } from "https://deno.land/std@0.112.0/datetime/mod.ts";
+
+import cloneDeep from "https://raw.githubusercontent.com/lodash/lodash/4.17.21-es/cloneDeep.js";
+export { cloneDeep };
+// または
+// export { default as cloneDeep } from "https://raw.githubusercontent.com/lodash/lodash/4.17.21-es/cloneDeep.js";
--- 003/sample5.ts
+++ 003/sample5.ts
@@ -1,4 +1,5 @@
-import { format, cloneDeep } from "./deps.ts";
+import { format } from "std-datetime";
+import cloneDeep from "lodash/cloneDeep.js";
 
 const today = new Date();
 
