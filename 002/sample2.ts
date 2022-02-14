4c4daf98ef8928af0794ddd153259aa877f98bcb
Kazuya HATTORI <boushi-bird@users.noreply.github.com>
2022-02-14 12:56:03 +0900

    ソースコードのあるディレクトリ以外から実行できるように修正

--- 002/sample2.ts
+++ 002/sample2.ts
@@ -1,2 +1,8 @@
-const text = await Deno.readTextFile("./assets/file.txt");
+// import については003/sample2.ts で解説
+import { dirname, resolve } from "https://deno.land/std@0.125.0/path/mod.ts";
+
+const dirPath = dirname(new URL(import.meta.url).pathname);
+const assetPath = resolve(dirPath, "./assets/file.txt");
+
+const text = await Deno.readTextFile(assetPath);
 console.log(text);
--- 002/sample3.ts
+++ 002/sample3.ts
@@ -1,3 +1,9 @@
-await Deno.writeTextFile("./dist/hello.txt", "Hello World!");
+// import については003/sample2.ts で解説
+import { dirname, resolve } from "https://deno.land/std@0.125.0/path/mod.ts";
+
+const dirPath = dirname(new URL(import.meta.url).pathname);
+const distPath = resolve(dirPath, "./dist/hello.txt");
+
+await Deno.writeTextFile(distPath, "Hello World!");
 
 console.log("File written to ./dist/hello.txt");
